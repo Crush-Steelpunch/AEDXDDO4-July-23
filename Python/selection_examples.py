27df15d801b968f96088b0df5d4fe91d82567664
Leon L. Robinson <leon.robinson@qa.com>
2023-09-11 14:37:38 +0100

    else and elif examples

--- Python/selection_examples.py
+++ Python/selection_examples.py
@@ -30,3 +30,23 @@ if inputvar == 7 or (inputvar > 12 and inputvar < 20):
 
 
 
+    # using else for returning information if the test is false
+
+if inputvar > 10:
+    print("You passed!")
+else:
+    print("You Failed!")
+    
+
+# Using elif to perform multiple sequential tests
+
+if inputvar < 10:
+    print("you have a single digit number")
+elif inputvar < 100:
+    print("you have a double digit number")
+elif inputvar < 1000:
+    print("You have a triple digit number")
+else:
+    print("Your number is huuuuge")
+
+print("end program")
