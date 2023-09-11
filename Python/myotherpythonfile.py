2ba53948648b5e2f03fb0f156176efe5009bd7ca
Leon L. Robinson <leon.robinson@qa.com>
2023-09-11 11:29:06 +0100

    Examples of variables and types

---
+++ Python/myotherpythonfile.py
@@ -0,0 +1,26 @@
+# this is a file for showing people how to do a python file
+
+print("Hello Class")    # This is a line that will be run
+
+#         0101110111
+# Binary representation of 4312858897
+# Could be a telebphone number
+# Could be a decimal value of money
+# Could be an ip address
+# 
+
+# Data typing 
+# - Integer, any whole number , e.g 1, -15 101,110,111
+# - Floating, any fractional number, e.g 1.0, -15.5. 101.110111
+# - String, alphanumeric text e.g. 'cat' 'Dinosaur2055', '01 110 1111'
+# - Boolean, true or false value e.g 'True', 'False'
+
+
+
+varname = 'value'  
+
+integervar = 123        # Integer
+floatvar = 123.45       # Float
+stringvar = "text here" # String
+boolvar = True          # Boolean, no quotes, capitalized words
+
