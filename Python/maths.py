f6cf8164b9d221357a5110df005fbd69f1f43cc6
Leon L. Robinson <leon.robinson@qa.com>
2023-09-12 09:51:28 +0100

    maths symbols

---
+++ Python/maths.py
@@ -0,0 +1,23 @@
+# Maths operators
+
+# Standard Symbols
+# +
+# -
+# /
+# * 
+
+# Other symbols
+
+# ** - to the power of
+
+#e.g
+
+intvar1 = 5
+intvar2 = 7
+
+print(intvar1 ** intvar2)
+
+# % - modulo
+
+
+
