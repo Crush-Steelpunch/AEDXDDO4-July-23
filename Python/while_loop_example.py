dc3905bd1e67fd2c75802f1364fd84320ad1e315
Leon L. Robinson <leon.robinson@qa.com>
2023-09-12 10:18:52 +0100

    continue example

--- Python/while_loop_example.py
+++ Python/while_loop_example.py
@@ -10,8 +10,7 @@
 x = 1
 while x <= 5:
     print(x,"Hello World")
-    x = x + 1
-
+    x = x + 1   # x += 1  short form of x = x + 1
 # slide example
 
 n = 1
@@ -22,3 +21,24 @@ print('*' * n)
 while n > 0:
     n = n - 1
     print('*' * n)
+
+
+# stopping loops on conditions
+
+inputvar1 = int(input("Type in a number: "))
+while inputvar1 > 0:
+    if inputvar1 == 13:
+        break          # Emergency Stop the loop
+    print(inputvar1)
+    inputvar1 = inputvar1 - 1
+
+
+# Skipping iterations on conditions
+print('continue example')
+inputvar1 = int(input("Type in a number: "))
+while inputvar1 > 0:
+    inputvar1 = inputvar1 - 1
+    if inputvar1 == 13:
+        continue          # Skipping a step in the loop
+    print(inputvar1)
+    
