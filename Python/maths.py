b8b1d3b2f683a2aab99e2200a68e77fd1c56e28d
Leon L. Robinson <leon.robinson@qa.com>
2023-09-12 09:56:26 +0100

    added modulo example

--- Python/maths.py
+++ Python/maths.py
@@ -19,5 +19,25 @@ print(intvar1 ** intvar2)
 
 # % - modulo
 
+# divides one number by another
+# returns only the remainder
+
+# 10 / 5 = 2.0 
+# 10 / 3 = 3 1/3  
+
+print(10 % 5)
+print( 10 % 3)
+
+# finding an even number
+
+testnum = int(input("Type a number: "))
+evenorodd = testnum % 2
+print("evenorodd is:", evenorodd)
+if evenorodd == 0:
+    print("it is an even number")
+else:
+    print("it is an odd number")
+
+
 
 
