5594d6efbade5516fc9ab00526f55bef34837ba9
Leon L. Robinson <leon.robinson@qa.com>
2023-09-13 10:23:02 +0100

    different ways to import

--- Python/library_functions_example.py
+++ Python/library_functions_example.py
@@ -1,5 +1,9 @@
-import math
+import math   # import the module
 
-sqrtof9 = math.sqrt(9)
+sqrtof9 = math.sqrt(9)   # access the function with modname.funcname()
 print(sqrtof9)
 
+from string import capwords  # import just the function from the module
+
+somewords = input("Type some words: ")
+print(capwords(somewords))    # call the function
