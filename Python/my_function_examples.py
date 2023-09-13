a4a514619f8660942563403b66ccc6aa9c64f31c
Leon L. Robinson <leon.robinson@qa.com>
2023-09-13 12:10:34 +0100

    Function with Input

---
+++ Python/my_function_examples.py
@@ -0,0 +1,26 @@
+# Writing your own functions
+
+def functionName():
+    #code bit here
+    pass   # This `pass` keyword lets you define a function with no actual code
+
+
+def showTwo():   # This defines the function
+    print(2)
+
+
+
+
+showTwo()    # This calls the function, to run it
+
+
+
+def addTwo(inputvar):
+    if inputvar.isnumeric():
+        print(int(inputvar) + 2)
+    else:
+        print("You didn't give me a number!")
+
+mynumber = input("Gimme number!: ")
+
+addTwo(mynumber)
