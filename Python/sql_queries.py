4497f5efdd7b0cc0aebdbc4a66ae4041f763ebf6
Leon L. Robinson <leon.robinson@qa.com>
2023-09-14 13:53:00 +0100

    Added cool queries that I didn't qrite

--- Python/sql_queries.py
+++ Python/sql_queries.py
@@ -44,8 +44,20 @@ sqlqueryvar = 'SELECT * FROM company ORDER BY company_no DESC'  # Select (ALL) F
 # Aggregates are one of min(),max(),sum(),avg(),count()
 
 sqlqueryvar = 'SELECT  emp_no, sum(order_value), count(emp_no) FROM  sale GROUP BY emp_no'
+#arti
+sqlqueryvar = 'SELECT first_name, post_code FROM salesperson where post_code not in (\'\')'
 
+#Ashley
 
+sqlqueryvar = "SELECT emp_no, count(order_date) FROM sale WHERE order_date BETWEEN '2000-04-01' AND '2000-06-30' GROUP BY emp_no"
+
+# the ; charactor is like an end-of-statement or line end
+
+sqlqueryvar = "DECLARE @minLivingWage AS DECIMAL = 13.000 ; SELECT emp_no, first_name, salary FROM salesperson WHERE county in ('surrey','london') and salary <= @minLivingWage"
+
+# Claire
+
+sqlqueryvar = "select sp.first_name, sp.last_name, sp.salary, sp.sales_target, s.order_no, s.order_value, s.order_date from  [salesperson] sp   inner join [sale] s  on sp.emp_no = s.emp_no order by sp.first_name, sp.last_name, s.order_date, s.order_value desc"
 
 resultvar = sql_query(sqlqueryvar)
 
